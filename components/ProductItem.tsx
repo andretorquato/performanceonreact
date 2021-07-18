62d95658e8a040c63388cb3d36ed44067e419eb8
André Torquato <andretorquato@alu.ufc.br>
2021-07-18 16:40:31 -0300

    :sparkles:feat: apply useCallback

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -5,11 +5,17 @@ interface ProductItemsProps {
     price: number;
     title: string;
   };
+  onAddToWishList: (id: number) => void;
 }
-function ProductItemComponent({ product }: ProductItemsProps) {
+function ProductItemComponent({ product, onAddToWishList }: ProductItemsProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
+      <button
+        type="button"
+        role="button"
+        onClick={() => onAddToWishList(product.id)}
+      > Add to wishlist</button>
     </div>
   );
 }
