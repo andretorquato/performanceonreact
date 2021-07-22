faa08dc1faea36366596a62c6e7ba721467aa07f
André Torquato <andretorquato@alu.ufc.br>
2021-07-21 22:37:07 -0300

    :zap:perf: use virtualization

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,5 @@
+import { List, ListRowRenderer } from "react-virtualized";
 import { ProductItem } from "./ProductItem";
-
 interface SearchResultsProps {
   results: Array<{
     id: number;
@@ -16,18 +16,27 @@ export function SearchResults({
   totalPrice,
   onAddToWishList,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
   return (
     <div>
-      <div>{totalPrice}</div>
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-          />
-        );
-      })}
+      <h2>{totalPrice}</h2>
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
